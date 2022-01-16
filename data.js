7a070e9583f70ca84f92893beb6ccda90c84f6dd
Jaikishore <belie@LAPTOP-6U13FRRC>
2022-01-16 12:09:08 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,47 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDj2AQHspdig-Qm2P_5KuDTpoPscLR1JsM",
+  authDomain: "fest-fe63e.firebaseapp.com",
+  projectId: "fest-fe63e",
+  storageBucket: "fest-fe63e.appspot.com",
+  messagingSenderId: "875515957315",
+  appId: "1:875515957315:web:7b9599e5a12c5c62a684f6"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+// let's code 
+var datab  = firebase.database().ref('data');
+function UserRegister(){
+var email = document.getElementById('eemail').value;
+var password = document.getElementById('lpassword').value;
+firebase.auth().createUserWithEmailAndPassword(email,password).then(function(){
+    
+}).catch(function (error){
+    var errorcode = error.code;
+    var errormsg = error.message;
+});
+}
+const auth = firebase.auth();
+function SignIn(){
+    var email = document.getElementById('eemail').value;
+    var password = document.getElementById('lpassword').value;
+    const promise = auth.signInWithEmailAndPassword(email,password);
+    promise.catch( e => alert(e.msg));
+    window.open("https://www.google.com","_self");
+}
+document.getElementById('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    var userInfo = datab.push();
+    userInfo.set({
+        name: getId('fname'),
+        email : getId('eemail'),
+        password : getId('lpassword')
+    });
+    alert("Successfully Signed Up");
+    console.log("sent");
+    document.getElementById('form').reset();
+});
+function  getId(id){
+    return document.getElementById(id).value;
+}
